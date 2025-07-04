78ef2d25ff827ae7aae608dac33f1549b4611d07
Gio-Midonz-Bots <roboticagiovanna@gmail.com>
2025-07-04 17:52:41 -0300

    Update: Class Principal

--- Screenmatch/src/main/java/br/com/Spring/Alura/Screenmatch/ScreenmatchApplication.java
+++ Screenmatch/src/main/java/br/com/Spring/Alura/Screenmatch/ScreenmatchApplication.java
@@ -1,13 +1,21 @@
 package br.com.Spring.Alura.Screenmatch;
 
+import br.com.Spring.Alura.Screenmatch.service.ConsumoAPI;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenmatchApplication {
+public class ScreenmatchApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoAPI();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?i=tt3896198&apikey=e0966007");
+		System.out.println(json);
+	}
 }
